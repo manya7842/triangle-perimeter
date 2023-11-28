b1cdb2f054e6a184f714469a67c71cee437ca247
manya7842 <manya7842>
2023-11-29 02:44:20 +0530

    h

---
+++ h.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class h
+{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the three sides of the triangle");
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+        System.out.println("The perimeter is: "+(a+b+c));
+    }
+}
